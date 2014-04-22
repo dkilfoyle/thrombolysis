2ea35d6740db2eff3ba4b2e6630156e23da814f4
Dean <dkilfoyle@xtra.co.nz>
2014-04-22 23:37:59 +1200

    keep html contents of mcq answers

--- js/mcq.js
+++ js/mcq.js
@@ -1,7 +1,7 @@
 function getFeedback(optionId, optionsNum, qid, mode) {
-    for (i = 1; i<= optionsNum; i++) { 
-		id = qid + "ans" + i
-        if(i == optionId)
+    for (i = 1; i <= optionsNum; i++) {
+        id = qid + "ans" + i
+        if (i == optionId)
             document.getElementById(id).style.display = "block";
         else
             document.getElementById(id).style.display = "None";
@@ -10,35 +10,37 @@ function getFeedback(optionId, optionsNum, qid, mode) {
 
 function convertMCQs() {
 
-  $(".MCQ").each(function(index, value) {
-  var qoptions = [];
-  var qans = [];
+    $(".MCQ").each(function (index, value) {
+        var qoptions = [];
+        var qans = [];
 
-  $(this).next().children().each(function(index, value) { 
-    if (index % 2 ==0)
-      qoptions.push($(this).text()) 
-    else
-      qans.push($(this).text())
-  });
+        $(this).next().children().each(function (index, value) {
+            if (index % 2 == 0)
+                qoptions.push($(this).html())
+            else
+                qans.push($(this).html())
+        });
 
-  $(this).next().remove();
-  
-  var mcqid = $(this).attr("id");
-  
-  var output="<div class='MCQ note'>\n<h5>" + $(this).text() + "</h5>\n<div class='MCQoptions'>\n";
-  for (i = 0; i < qoptions.length; i++) {
-	 output = output + "<input type='radio' name='" + mcqid + "' onclick='getFeedback(" + (i+1) + ", " + qoptions.length + ", &quot;" + mcqid + "&quot;)'><label>" + qoptions[i] +  "</label><br>\n";
-  }
-  output = output + "</div>\n";
-  
-  var output = output + "<div class='MCQanswers'>\n";
-  for (i = 0; i < qans.length; i++) {
-	 output = output + "<div id='" + mcqid + "ans" + (i+1) + "' style='display:none'>" + qans[i] + "</div>";
-	}
-  output = output + "</div>\n<div>Select the single best answer.</div>\n</div>\n";
+        $(this).next().remove();
 
-  $(this).replaceWith($(output));
-})
+        var mcqid = $(this).attr("id");
+
+        var output = "<div class='MCQ note'>\n<h5>" + $(this).text() + "</h5>\n<div class='MCQoptions'>\n";
+        for (i = 0; i < qoptions.length; i++) {
+            output = output + "<input type='radio' name='" + mcqid + "' onclick='getFeedback(" + (i + 1) + ", " + qoptions.length + ", &quot;" + mcqid + "&quot;)'><label>" + qoptions[i] + "</label><br>\n";
+        }
+        output = output + "</div>\n";
+
+        var output = output + "<div class='MCQanswers'>\n";
+        for (i = 0; i < qans.length; i++) {
+            output = output + "<div id='" + mcqid + "ans" + (i + 1) + "' style='display:none'>" + qans[i] + "</div>";
+        }
+        output = output + "</div>\n<div>Select the single best answer.</div>\n</div>\n";
+
+        $(this).replaceWith($(output));
+    })
 }
 
-$(document).ready(function() { convertMCQs(); });
+$(document).ready(function () {
+    convertMCQs();
+});
